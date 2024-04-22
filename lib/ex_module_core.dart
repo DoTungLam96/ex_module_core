f2d1f5b61e1972c4525efe09aaf3c0f7a8fb8e4d
lamdo <tunglamdo96@gmail.com>
2024-04-22 23:35:28 +0700

    init

--- lib/ex_module_core.dart
+++ lib/ex_module_core.dart
@@ -30,3 +30,4 @@ export 'core/usecase.dart';
 export 'package:get_it/get_it.dart';
 export 'package:dio/dio.dart';
 export 'package:retrofit/retrofit.dart' hide Headers;
+export 'package:flutter_bloc/flutter_bloc.dart';
